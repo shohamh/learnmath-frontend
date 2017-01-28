5977870f2afce1769883790444ba6a095dad6316
shohamh <shohamh@hotmail.com>
2017-01-28 23:42:11 +0200

    Added layout from mdl, trying it now

--- Main.elm
+++ Main.elm
@@ -3,7 +3,7 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (href, class, style)
 import Material
-import Material.Scheme
+import Material.Layout as Layout
 import Material.Button as Button
 import Material.Options as Options exposing (css)
 
@@ -70,38 +70,15 @@ type alias Mdl =
 
 view : Model -> Html Msg
 view model =
-    div
-        [ style [ ( "padding", "2rem" ) ] ]
-        [ text ("Current count: " ++ toString model.count)
-          {- We construct the instances of the Button component that we need, one
-             for the increase button, one for the reset button. First, the increase
-             button. The first three arguments are:
-               - A Msg constructor (`Mdl`), lifting Mdl messages to the Msg type.
-               - An instance id (the `[0]`). Every component that uses the same model
-                 collection (model.mdl in this file) must have a distinct instance id.
-               - A reference to the elm-mdl model collection (`model.mdl`).
-             Notice that we do not have to add fields for the increase and reset buttons
-             separately to our model; and we did not have to add to our update messages
-             to handle their internal events.
-             Mdl components are configured with `Options`, similar to `Html.Attributes`.
-             The `Options.onClick Increase` option instructs the button to send the `Increase`
-             message when clicked. The `css ...` option adds CSS styling to the button.
-             See `Material.Options` for details on options.
-          -}
-        , Button.render Mdl
-            [ 0 ]
-            model.mdl
-            [ Options.onClick Increase
-            , css "margin" "0 24px"
-            ]
-            [ text "Increase" ]
-        , Button.render Mdl
-            [ 1 ]
-            model.mdl
-            [ Options.onClick Reset ]
-            [ text "Reset" ]
+    Layout.render Mdl
+        model.mdl
+        [ Layout.fixedHeader
         ]
-        |> Material.Scheme.top
+        { header = [ text "header text" ]
+        , drawer = [ text "drawer text" ]
+        , tabs = ( [], [] )
+        , main = [ text "main text" ]
+        }
 
 
 
@@ -113,8 +90,11 @@ view model =
 main : Program Never Model Msg
 main =
     Html.program
-        { init = ( model, Cmd.none )
+        { init =
+            ( { model | mdl = Layout.setTabsWidth 1384 model.mdl }
+            , Layout.sub0 Mdl
+            )
         , view = view
-        , subscriptions = always Sub.none
+        , subscriptions = .mdl >> Layout.subs Mdl
         , update = update
         }
